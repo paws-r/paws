0148cacb4cf73fdb8e4bac9e384f12f54af07124
Adam Banker <adam.banker39@gmail.com>
2018-11-11 14:23:56 -0500

    updated s3 customizations

--- make.paws/R/customizations/s3.R
+++ make.paws/R/customizations/s3.R
@@ -1,36 +1,39 @@
+#' @include paws.s3_service.R
+NULL
+
 ################################################################################
 # update_endpoint_for_s3_config
   
 bucket_name_from_req_params <- function(request) {
   request_params <- request$params
   bucket <- request_params["Bucket"]
-  
+
   if (is.null(bucket)) return(NULL)
-  
+
   bucket_name <- bucket[[1]]
-  
+
   return(bucket_name)
 }
 
 move_bucket_to_host <- function(url, bucket) {
   url$host <- paste0(bucket, ".", url$host)
   url$path <- gsub("/\\{Bucket\\}", "", url$path)
-  
+
   if (url$path == "") {
     url$path = "/"
   }
-  
+
   return(url)
 }
 
 update_endpoint_for_s3_config <- function(request) {
   bucket_name <- bucket_name_from_req_params(request)
-  
+
   if (is.null(bucket_name)) return(NULL)
-  
+
   request$http_request$url <-
     move_bucket_to_host(request$http_request$url, bucket_name)
-  
+
   return(request)
 }
 
@@ -38,24 +41,23 @@ update_endpoint_for_s3_config <- function(request) {
 # contentMD5
 
 content_md5 <- function(request) {
-  
+
   body <- request$body
-  
+
   hash <- digest::digest(body, serialize = FALSE, raw = TRUE)
-  
+
   base64_hash <- base64enc::base64encode(hash)
-  
+
   request$http_request$header$`Content-Md5` <- base64_hash
-  
+
   return(request)
-  
+
 }
 
-content_length <- function(request) {
-  
-  body <- request$http_request$body
-  
-  request$http_request$content_length <- nchar(body)
-  
-  return(request)
-}
+################################################################################
+
+HANDLERS$build <- add_handlers_front(HANDLERS$build,
+                                     update_endpoint_for_s3_config)
+
+HANDLERS$build <- add_handlers_back(HANDLERS$build,
+                                     content_md5)
