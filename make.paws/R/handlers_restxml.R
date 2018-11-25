0e6681c488017f5533fb4f9ff84a0e6675955859
David Kretch <david.kretch@gmail.com>
2018-11-25 07:09:09 -0500

    Add placeholder restxml handlers

---
+++ make.paws/R/handlers_restxml.R
@@ -0,0 +1,22 @@
+# Build a request for the REST XML protocol.
+restxml_build <- function(request) {
+  return(request)
+}
+
+#-------------------------------------------------------------------------------
+
+# Unmarshal REST XML protocol API response metadata.
+restxml_unmarshal_meta <- function(request) {
+  return(request)
+}
+
+# Unmarshal the body from a REST XML protocol API response.
+restxml_unmarshal <- function(request) {
+  return(request)
+}
+
+# Unmarshal an error from a REST XML protocol API response.
+restxml_unmarshal_error <- function(request) {
+  return(request)
+}
+
