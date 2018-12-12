66b3de0801e1095fb57250293bf2e7d30b21bf38
David Kretch <david.kretch@gmail.com>
2018-12-11 21:45:41 -0500

    Set up an IAM role for the Batch example

--- examples/batch.R
+++ examples/batch.R
@@ -1,7 +1,11 @@
 # Batch examples
 
+# Note: In some cases, you'll need to wait for a step to complete in your AWS
+# account before you can successfully run the next.
+
 # To set up the Batch compute environment, get security group and subnet info
 # for the default VPC.
+
 default_vpc <- paws.ec2::describe_vpcs(
   Filters = "isDefault=true"
 )$Vpcs[[1]]
@@ -15,12 +19,37 @@ subnets <- paws.ec2::describe_subnets(
   Filters = sprintf("vpc-id=%s", default_vpc$VpcId)
 )$Subnets
 
-# You will need to set up the Batch service role and provide your AWS account
-# account number here.
-service_role <- sprintf(
-  "arn:aws:iam::%s:role/service-role/AWSBatchServiceRole",
-  account_id = "1234567890123"
+#-------------------------------------------------------------------------------
+
+# Set up an IAM role for the Lambda function.
+
+role_name <- "TestBatchServiceRole"
+policy_arn <- "arn:aws:iam::aws:policy/service-role/AWSBatchServiceRole"
+
+role_policy <- list(
+  Version = "2012-10-17",
+  Statement = list(
+    list(
+      Effect = "Allow",
+      Principal = list(
+        Service = "batch.amazonaws.com"
+      ),
+      Action = "sts:AssumeRole"
+    )
+  )
 )
+
+iam <- paws.iam::create_role(
+  RoleName = role_name,
+  AssumeRolePolicyDocument = jsonlite::toJSON(role_policy, auto_unbox = TRUE)
+)
+
+paws.iam::attach_role_policy(
+  RoleName = role_name,
+  PolicyArn = policy_arn
+)
+
+#-------------------------------------------------------------------------------
 
 # Set up a compute environment: the resources on which Batch jobs will run.
 paws.batch::create_compute_environment(
@@ -37,7 +66,7 @@ paws.batch::create_compute_environment(
     securityGroupIds = security_group$GroupId,
     subnets = sapply(subnets, function(x) x$SubnetId)
   ),
-  serviceRole = service_role,
+  serviceRole = iam$Role$Arn,
   state = "ENABLED"
 )
 
@@ -82,10 +111,11 @@ paws.batch::list_jobs(
   jobStatus = "SUBMITTED"
 )
 
-# Clean up.
+# Clean up. You may have to wait for some steps to complete.
 paws.batch::deregister_job_definition(jobDefinition = job_def$jobDefinitionArn)
 paws.batch::update_job_queue("TestJobQueue", state = "DISABLED")
 paws.batch::delete_job_queue("TestJobQueue")
-Sys.sleep(30)
 paws.batch::update_compute_environment("TestComputeEnvironment", state = "DISABLED")
 paws.batch::delete_compute_environment(computeEnvironment = "TestComputeEnvironment")
+paws.iam::detach_role_policy(role_name, policy_arn)
+paws.iam::delete_role(role_name)
