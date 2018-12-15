d06e2d7a834c724523532e7482457c0d1c8ca290
David Kretch <david.kretch@gmail.com>
2018-12-15 14:39:23 -0500

    Add shape builder tests

---
+++ make.paws/tests/testthat/test_populateutil.R
@@ -0,0 +1,41 @@
+context("Shape builders")
+
+test_that("Structure", {
+  foo <- Structure(a = 1, b = 2)
+  expect_equal(tag_get(foo, "type"), "structure")
+  expect_equivalent(foo, list(a = 1, b = 2))
+
+  expect_error(Structure(1, 2, 3))
+})
+
+test_that("List", {
+  foo <- List()
+  expect_equal(tag_get(foo, "type"), "list")
+
+  bar <- List(1, 2)
+  expect_equivalent(bar, list(1, 2))
+
+  expect_error(List(a = 1, b = 2))
+})
+
+test_that("Map", {
+  foo <- Map()
+  expect_equal(tag_get(foo, "type"), "map")
+
+  bar <- Map(list(Name = "foo", Value = 1), list(Name = "bar", Value = 2))
+  expect_equivalent(bar, list(list(Name = "foo", Value = 1), list(Name = "bar", Value = 2)))
+
+  expect_error(Map(a = 1, b = 2))
+})
+
+test_that("Scalar", {
+  foo <- Scalar()
+  expect_equal(tag_get(foo, "type"), "scalar")
+
+  bar <- Scalar("foo")
+  expect_equivalent(bar, "foo")
+
+  expect_error(Scalar(c(1, 2)))
+  expect_error(Scalar(list()))
+  expect_error(Scalar(list(1)))
+})
