e9e069de07ab59fa823075f3fec57ac451c252cb
David Kretch <david.kretch@gmail.com>
2018-12-17 21:32:30 -0500

    Delete TODO: Implement comment for sdk_version_user_agent_handler

--- make.paws/R/handlers_core.R
+++ make.paws/R/handlers_core.R
@@ -1,5 +1,4 @@
 # Add the name/version to the User-Agent request header.
-# TODO: Implement.
 sdk_version_user_agent_handler <- function(request) {
   paws_version <- utils::packageVersion(methods::getPackageName())
   r_version <- getRversion()
@@ -7,7 +6,6 @@ sdk_version_user_agent_handler <- function(request) {
   r_arch <- R.Version()$arch
   user_agent <- sprintf("paws/%s (R%s; %s; %s)",
                         paws_version, r_version, r_os, r_arch)
-  
   request$http_request$header["User-Agent"] <- user_agent
   return(request)
 }
