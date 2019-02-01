df326426719c2319e9341408fa11f941c932c391
David Kretch <david.kretch@gmail.com>
2019-01-31 19:31:02 -0500

    Add tests for handler helpers and exported service/handler fns

--- paws.common/tests/testthat/test_handlers.R
+++ paws.common/tests/testthat/test_handlers.R
@@ -25,3 +25,23 @@ test_that("run multiple handlers", {
   r <- run(r, l)
   expect_null(r$data)
 })
+
+test_that("setting and adding handlers", {
+  handlers <- Handlers()
+  f <- function(x) 1
+  handlers$validate <- handlers_set(f)
+  expect_equal(handlers$validate$list[[1]]$fn, f)
+
+  g <- function(x) 2
+  handlers$validate <- handlers_add_back(handlers$validate, g)
+  expect_equal(length(handlers$validate$list), 2)
+  expect_equal(handlers$validate$list[[1]]$fn, f)
+  expect_equal(handlers$validate$list[[2]]$fn, g)
+
+  h <- function(x) 3
+  handlers$validate <- handlers_add_front(handlers$validate, h)
+  expect_equal(length(handlers$validate$list), 3)
+  expect_equal(handlers$validate$list[[1]]$fn, h)
+  expect_equal(handlers$validate$list[[2]]$fn, f)
+  expect_equal(handlers$validate$list[[3]]$fn, g)
+})
