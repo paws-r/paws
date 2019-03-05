c93ae9234fe94d46466803293dd54f0734cdbad7
David Kretch <david.kretch@gmail.com>
2019-03-04 22:11:03 -0500

    Avoid un-escaping brackets and backticks

--- make.paws/R/docs.R
+++ make.paws/R/docs.R
@@ -207,10 +207,15 @@ clean_html_node <- function(node) {
 
 # Escape special characters % { }, and single \ not followed by another special
 # character. These cause problems in R documentation Rd files.
+# Do not escape \ when followed by:
+#   1. % { } \ ' " ` -- escaped special Rd or LaTeX characters
+#   2. * [ ] -- escaped markdown characters
+#   3. ~ -- ???
 # See https://developer.r-project.org/parseRd.pdf.
 escape_special_chars <- function(text) {
   result <- text
-  result <- gsub("(?<!\\\\)\\\\(?![\\\\%{}'\"\\*~])", "\\\\\\\\", result, perl = TRUE)
+  result <- gsub("(?<!\\\\)\\\\(?![\\\\%{}'\"`\\*~\\[\\]])", "\\\\\\\\", result, perl = TRUE)
+  result <- gsub("`\\\\`", "`\\\\\\\\`", result, fixed = TRUE) # Special case: `\`
   for (char in c("%", "{", "}")) {
     result <- gsub(paste0("(?<!\\\\)", char), paste0("\\\\", char), result, perl = TRUE)
   }
