50ecce43b2553caa3c6d5c674ae9db041b43a292
David Kretch <david.kretch@gmail.com>
2019-03-06 21:40:32 -0500

    Delete get_structure test that doesn't work cross-platform

--- make.paws/tests/testthat/test_utils.R
+++ make.paws/tests/testthat/test_utils.R
@@ -5,10 +5,6 @@ test_that("get_structure", {
   expect_equal(get_structure(list(a = 1, b = 2)), "list(a = 1, b = 2)")
   expect_equal(get_structure(c(1, 2, 3)), "c(1, 2, 3)")
 
-  got <- get_structure(as.POSIXct("1970-01-01"))
-  want <- "structure(18000, class = c(\"POSIXct\", \"POSIXt\"), tzone = \"\")"
-  expect_equal(got, want)
-
   obj <- paws.common::tag_add(list(a = 1, b = 2), list(type = "structure"))
   got <- get_structure(obj)
   want <- "structure(list(a = 1, b = 2), tags = list(type = \"structure\"))"
