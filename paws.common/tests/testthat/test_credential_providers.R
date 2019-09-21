efc57c69a9c9d395d859b5cf47fdbc14e06edb5f
David Kretch <david.kretch@summitllc.us>
2019-09-21 09:51:17 -0400

    Add session token tests

--- paws.common/tests/testthat/test_credential_providers.R
+++ paws.common/tests/testthat/test_credential_providers.R
@@ -10,8 +10,19 @@ test_that("r_env_provider", {
   expect_equal(creds$secret_access_key, "bar")
 
   Sys.setenv(
+    "AWS_ACCESS_KEY_ID" = "foo",
+    "AWS_SECRET_ACCESS_KEY" = "bar",
+    "AWS_SESSION_TOKEN" = "foobar"
+  )
+  creds <- r_env_provider()
+  expect_equal(creds$access_key_id, "foo")
+  expect_equal(creds$secret_access_key, "bar")
+  expect_equal(creds$session_token, "foobar")
+
+  Sys.setenv(
     "AWS_ACCESS_KEY_ID" = "",
-    "AWS_SECRET_ACCESS_KEY" = ""
+    "AWS_SECRET_ACCESS_KEY" = "",
+    "AWS_SESSION_TOKEN" = ""
   )
   creds <- r_env_provider()
   expect_null(creds)
