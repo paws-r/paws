06aa6e491d3ded1584fac250280ee739dee2786d
David Kretch <david.kretch@gmail.com>
2019-10-16 22:52:09 -0400

    Handle null profile names when getting region (#217)
    * Handle null profile names when getting region. This comes up when using an old paws package with the new paws.common.

--- paws.common/tests/testthat/test_config.R
+++ paws.common/tests/testthat/test_config.R
@@ -53,3 +53,16 @@ test_that("set_config", {
 
   expect_error(set_config(list(), list(foo = 123)))
 })
+
+test_that("get_region", {
+  Sys.setenv(AWS_REGION = "foo")
+  expect_equal(get_region(), "foo")
+  expect_equal(get_region(NULL), "foo")
+})
+
+test_that("get_profile_name", {
+  Sys.setenv(AWS_PROFILE = "bar")
+  expect_equal(get_profile_name(), "bar")
+  expect_equal(get_profile_name(NULL), "bar")
+  expect_equal(get_profile_name("foo"), "foo")
+})
