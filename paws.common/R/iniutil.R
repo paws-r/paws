df654f9378d4fe0848edb25254b067c813376e9a
Adam Banker <adam.banker39@gmail.com>
2021-03-13 18:52:38 -0500

    Read INI files, remove `ini` dependency (#394)
    * Add new `read_ini` function for reading in INI files. Remove the `ini` package dependency in `paws.common`.

---
+++ paws.common/R/iniutil.R
@@ -0,0 +1,37 @@
+# Get the profile name from an ini file
+extract_ini_profile <- function(item) {
+  profile <- gsub("\\[|\\]", "", item)
+  return(profile)
+}
+
+# Get a parameter and its value
+extract_ini_parameter <- function(item) {
+  split_index <- regexpr("=", item)
+  parameter <- list()
+  key <- substring(item, 1, split_index)
+  value <- substring(item, split_index + 1)
+  parameter[[trimws(key)]] <- trimws(value)
+  return(parameter)
+}
+
+# Read in values from an ini file
+read_ini <- function(file_name) {
+  if (!file.exists(file_name)) {
+    stop(sprintf("Unable to find file: %s", file_name))
+  }
+  content <- scan(file_name, what = "", sep = "\n", quiet = T)
+  profiles <- list()
+  current_profile <- ""
+  for (i in 1:length(content)) {
+    if (grepl("(^;)|(^#)", content[i])) {
+      next
+    } else if (grepl("^\\[.*\\]", content[i])) {
+      current_profile <- extract_ini_profile(content[i])
+      profiles[[current_profile]] <- c()
+      next
+    }
+    parameter <- extract_ini_parameter(content[i])
+    profiles[[current_profile]] <- c(profiles[[current_profile]], parameter)
+  }
+  return(profiles)
+}
---
+++ paws.common/tests/testthat/test_iniutil.R
@@ -0,0 +1,33 @@
+test_that("Reads in default profile", {
+  content <- read_ini("data_ini")
+  profile <- "default"
+  expect_equal(content[[profile]]$arg1, "value1")
+  expect_equal(content[[profile]]$arg2, "value2")
+})
+
+test_that("Reads in alternative profile", {
+  content <- read_ini("data_ini")
+  profile <- "foo"
+  expect_equal(content[[profile]]$arg1, "foo_value1")
+  expect_equal(content[[profile]]$arg2, "foo_value2")
+})
+
+test_that("Ignores lines starting with # and ;", {
+  content <- read_ini("data_ini")
+  expect_true(!("ignore1" %in% names(content)))
+  expect_true(!("ignore2" %in% names(content)))
+})
+
+test_that("Reads in profile with space in name", {
+  content <- read_ini("data_ini")
+  profile <- "profile bar"
+  expect_equal(content[[profile]]$arg1, "bar_value1")
+  expect_equal(content[[profile]]$arg2, "bar_value2")
+})
+
+test_that("Reads in values with equal signs", {
+  content <- read_ini("data_ini")
+  profile <- "equalsign"
+  expect_equal(content[[profile]]$arg1, "value1==")
+  expect_equal(content[[profile]]$arg2, "value2")
+})
