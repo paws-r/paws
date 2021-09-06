dfe11dc8656524f0cdede9f74ed83fb7bd6af974
Dyfan Jones <dyfan.jones@theverygroup.com>
2021-09-06 14:27:24 +0200

    unit test: add check of get_config when paws higherlevel function is ran in a do.call (#449)

--- paws.common/tests/testthat/test_config.R
+++ paws.common/tests/testthat/test_config.R
@@ -8,6 +8,8 @@ test_that("get_config", {
 
   actual <- svc$operation()
   expect_equivalent(actual$region, 123)
+  # check if config is returned when executed in a do.call
+  expect_equivalent(do.call(svc$operation, list()), svc$operation())
 
   f <- function() {
     svc$operation()
