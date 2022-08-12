c72267577fb62a1f6af9af1670ec1855ed851226
dyfan.jones <23235152+DyfanJones@users.noreply.github.com>
2022-08-12 20:06:29 +0200

    helper functions to identify aws services and operations

---
+++ make.paws/R/aws_services.R
@@ -0,0 +1,51 @@
+#' @include make_sdk.R read_api.R names.R cran_category.R
+NULL
+
+#' @import data.table
+
+#' Helper function to list all aws services and operations from awds js sdk
+#'
+#' @param in_dir Directory containing aws js sdk.
+#'
+#' @export
+list_aws_services <- function(in_dir = "../vendor/aws-sdk-js"){
+  apis <- list_apis(file.path(in_dir, "apis"))
+  aws_service_ops <- lapply(apis, function(x) {
+    api <- read_api(x, in_dir)
+    data.frame(
+      services = package_name(api),
+      operations = vapply(api$operations, function(x) {
+        get_operation_name(x)
+      }, FUN.VALUE = "", USE.NAMES = F)
+    )
+  })
+  return(do.call(rbind, aws_service_ops))
+}
+
+#' Helper function to count all operations by service and category
+#'
+#' @param in_dir Directory containing aws js sdk.
+#'
+#' @export
+category_service_ops_count <- function(in_dir = "../vendor/aws-sdk-js"){
+   aws_service_ops <- list_aws_services(in_dir)
+   paws_cat <- lapply(get_categories(), function(x) {
+     data.frame(name = rep(x$name, length(x$services)), services = x$services)
+   })
+   paws_cat <- rbindlist(paws_cat, fill = T)
+   paws_cat_service_ops <- merge(paws_cat, aws_service_ops, by = "services", all.y = T)
+   setcolorder(paws_cat_service_ops, "name")
+   names(paws_cat_service_ops) <- c("category", "services", "operations")
+   paws_cat_service_ops_count <- paws_cat_service_ops[, .(
+     total_operations = .N
+   ), by = c("category", "services")][, .(
+     services = get("services"),
+     total_services = uniqueN(get("services")),
+     total_operations = get("total_operations"),
+     sum_operations = sum(get("total_operations"))
+    ), by = "category"]
+   paws_cat_service_ops_count <- paws_cat_service_ops_count[
+     order(get("category"), -get("total_services"), -get("total_operations"))
+   ]
+   return(paws_cat_service_ops_count)
+}
