201128ee45b7a0a59a75ac3ec7c900a368a34cc5
dyfan.jones <23235152+DyfanJones@users.noreply.github.com>
2022-08-12 20:04:43 +0200

    build categories from sub-categories

--- make.paws/R/cran_collection.R
+++ make.paws/R/cran_collection.R
@@ -1,7 +1,9 @@
 #' @include templates.R package.R cran_category.R
 NULL
 
-#' Make packages for CRAN
+#' @importFrom stats setNames
+
+#' @title Make packages for CRAN
 #'
 #' @param sdk_dir Directory containing the output of `make_sdk`.
 #' @param out_dir Directory to write the packages to.
@@ -12,7 +14,88 @@ NULL
 make_cran <- function(sdk_dir, out_dir, categories = NULL, only_cran = FALSE) {
   if (is.null(categories)) categories <- get_categories()
   make_categories(sdk_dir, out_dir, categories)
+
+  # Identify sub-categories
+  found <- find_sub_categories(categories)
+
+  # Group categories
+  grp_sub_cats <- group_categories(categories[found])
+
+  # Build categories from sub-categories
+  for(cat in names(grp_sub_cats)){
+    make_category_collection(sdk_dir, out_dir, grp_sub_cats[[cat]], cat)
+  }
+
+  # rebuild categories from sub-categories for paws
+  categories <- make_category_from_sub_category(categories)
   make_collection(sdk_dir, out_dir, categories, only_cran)
+}
+
+# Make the categories from collection of sub-categories
+make_category_collection <- function(sdk_dir, out_dir, categories, package) {
+  version <- get_version(sdk_dir)
+  package <- sprintf("paws.%s", package)
+  package_dir <- file.path(out_dir, package)
+  write_skeleton_category(package_dir)
+  write_description_category(
+    package_dir,
+    package,
+    title = unique(unlist(sapply(categories, function(cat) cat$title))),
+    description = paste0(
+      lapply(categories, function(cat) cat$category_description), collapse = ""
+    ),
+    version = version,
+    imports = c()
+  )
+  # Create packages that contain services
+  active <- vapply(categories, function(cat) {
+    !is.null(cat$services)
+    }, FUN.VALUE = logical(1)
+  )
+  categories <- categories[active]
+  write_source_collection(sdk_dir, package_dir, categories)
+  write_documentation(package_dir)
+  write_imports_collection(package_dir, version, get_category_packages(categories))
+}
+
+# Identify sub-categories
+find_sub_categories <- function(categories){
+  return(vapply(categories, function(cat) {
+      !is.null(cat$category_description)
+    }, logical(1))
+  )
+}
+
+# Group sub-categories
+group_categories <- function(categories){
+  grp <- sapply(categories, function(cat) {
+    gsub(".p[0-9]+$", "", cat$name)
+  })
+  return(split(categories, grp))
+}
+
+make_category_from_sub_category <- function(categories){
+  found <- find_sub_categories(categories)
+  sub_categories <- categories[found]
+  grp_sub_cats <- group_categories(sub_categories)
+  cats <- names(grp_sub_cats)
+  # Build each categories metadata from sub-categories
+  cat_meta <- setNames(vector(mode = "list", length = length(cats)), cats)
+  for (cat in cats){
+    cat_meta[[cat]]$name <- cat
+    cat_meta[[cat]]$services <- unlist(sapply(
+      sub_categories[[cat]], function(sub_cat) sub_cat$services
+    ))
+    cat_meta[[cat]]$title <- unique(unlist(sapply(
+      sub_categories[[cat]], function(sub_cat) sub_cat$title
+    )))
+    cat_meta[[cat]]$description <- paste0(
+      lapply(sub_categories[[cat]], function(sub_cat) {
+        sub_cat$category_description
+      }), collapse = ""
+    )
+  }
+  return(c(categories[!found], unname(cat_meta)))
 }
 
 # Make the package which collects all the category packages.
