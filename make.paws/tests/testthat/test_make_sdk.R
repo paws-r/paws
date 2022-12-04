0d3f3c63a4455a7a7dfcade255c3bd4141a1f944
David Kretch <david.kretch@gmail.com>
2022-12-04 18:40:41 -0500

    Fix test

--- make.paws/tests/testthat/test_make_sdk.R
+++ make.paws/tests/testthat/test_make_sdk.R
@@ -81,23 +81,24 @@ jsonlite::write_json(region_config, region_config_path, auto_unbox = TRUE)
 path_out <- file.path(path, "out")
 path_docs <- file.path(path, "docs")
 
-test_get_categories <- function() {
-  return(list(
+#-------------------------------------------------------------------------------
+
+test_that("make_sdk", {
+
+  categories <- list(
     list(
       name = "bar",
       services = list("foo"),
       title = "Foo",
       description = "This is a complete sentence."
     )
-  ))
-}
+  )
 
-# Override the categories for testing.
-assignInNamespace("get_categories", test_get_categories, "make.paws")
+  test_get_categories <- function() return(categories)
 
-#-------------------------------------------------------------------------------
+  # Override the categories for testing.
+  assignInNamespace("get_categories", test_get_categories, "make.paws")
 
-test_that("make_sdk", {
   make_sdk(path_in, path_out, path_docs, only_cran = FALSE)
 
   expect_true(dir.exists(path_out))
@@ -110,45 +111,49 @@ test_that("make_sdk", {
   expect_true(all(files %in% list.files(file.path(path_out, "paws.bar/R"))))
 })
 
-test_that("make_categories with sub categories", {
-   categories <- list(
-     list(
-       name = "bar.p1",
-       title = "Bar",
-       description = "Bar.",
-       category_description = "Category Bar.",
-       services = list(
-         "foo"
-       )
-     ),
-     list(
-       name = "bar.p2",
-       title = "Bar",
-       description = "Bar.",
-       category_description = "",
-       services = list(
-         "foo"
-       )
-     )
-   )
-   path_cran <- file.path(path, "cran")
-   dir.create(path_cran, showWarnings = FALSE)
-   # empty cran folder
-   file.remove(list.files(path_cran, full.names = T,recursive = T))
+test_that("make_sdk with sub categories", {
+  # Clear out files from previous tests.
+  file.remove(list.files(path_out, full.names = T, recursive = T))
 
-   expect_error(make_categories(path_out, path_cran, categories), NA)
-   expect_true(file.exists(file.path(path_cran, "paws")))
-   expect_true(file.exists(file.path(path_cran, "paws.bar")))
-   expect_true(file.exists(file.path(path_cran, "paws.bar.p1")))
-   expect_true(file.exists(file.path(path_cran, "paws.bar.p2")))
+  categories <- list(
+    list(
+      name = "bar.p1",
+      title = "Bar",
+      description = "Bar.",
+      category_description = "Category Bar.",
+      services = list(
+        "foo"
+      )
+    ),
+    list(
+      name = "bar.p2",
+      title = "Bar",
+      description = "Bar.",
+      category_description = "",
+      services = list(
+        "foo"
+      )
+    )
+  )
 
-   # Should continue to work a second time.
-   expect_error(make_cran(path_out, path_cran, categories), NA)
-   expect_true(file.exists(file.path(path_cran, "paws")))
-   expect_true(file.exists(file.path(path_cran, "paws.bar")))
-   expect_true(file.exists(file.path(path_cran, "paws.bar.p1")))
-   expect_true(file.exists(file.path(path_cran, "paws.bar.p2")))
- })
+  test_get_categories <- function() return(categories)
+
+  # Override the categories for testing.
+  assignInNamespace("get_categories", test_get_categories, "make.paws")
+
+  expect_error(make_sdk(path_in, path_out, path_docs, only_cran = FALSE), NA)
+  expect_true(file.exists(file.path(path_out, "paws")))
+  expect_true(file.exists(file.path(path_out, "paws.bar")))
+  expect_true(file.exists(file.path(path_out, "paws.bar.p1")))
+  expect_true(file.exists(file.path(path_out, "paws.bar.p2")))
+
+  # Should continue to work a second time.
+  expect_error(make_sdk(path_in, path_out, path_docs, only_cran = FALSE), NA)
+  expect_true(file.exists(file.path(path_out, "paws")))
+  expect_true(file.exists(file.path(path_out, "paws.bar")))
+  expect_true(file.exists(file.path(path_out, "paws.bar.p1")))
+  expect_true(file.exists(file.path(path_out, "paws.bar.p2")))
+})
 
 test_that("list_apis", {
   temp <- tempdir()
