defec4bde9e22dd6ba26379be69481a4328c7ac4
dyfan.jones <23235152+DyfanJones@users.noreply.github.com>
2022-12-22 17:17:58 +0000

    initial generate_presigned_url unit tests

---
+++ paws.common/tests/testthat/test_signer_query.R
@@ -0,0 +1,165 @@
+mock_request <- function(bucket, key, http_path) {
+  list(
+    operation = list(
+      http_path = http_path
+    ),
+    params = list(
+      Bucket = bucket,
+      Key = key
+    )
+  )
+}
+
+http_paths <- c(
+  "/{Bucket}",
+  "/{Bucket}?foo",
+  "/{Bucket}/{Key+}",
+  "/{Bucket}/{Key+}?bar"
+)
+
+test_that("get auth path for standard bucket and key", {
+  bucket <- "made-up"
+  key <- "file"
+  expect_out <- list(
+    sprintf("/%s/", bucket),
+    sprintf("/%s/", bucket),
+    sprintf("/%s/%s", bucket, key),
+    sprintf("/%s/%s", bucket, key)
+  )
+
+  for (i in seq_along(http_paths)) {
+    actual <- paws.common:::get_auth(mock_request(bucket, key, http_paths[i]))
+    expect_equal(actual, expect_out[[i]])
+  }
+})
+
+test_that("get auth path with special characters #1", {
+  bucket <- "made_up"
+  key <- "path/file#"
+  expect_out <- list(
+    sprintf("/%s", bucket),
+    sprintf("/%s", bucket),
+    sprintf("/%s/%s", bucket, paws.common:::paws_url_encoder(key, "/-")),
+    sprintf("/%s/%s", bucket, paws.common:::paws_url_encoder(key, "/~"))
+  )
+
+  for (i in seq_along(http_paths)) {
+    actual <- paws.common:::get_auth(mock_request(bucket, key, http_paths[i]))
+    expect_equal(actual, expect_out[[i]])
+  }
+})
+
+test_that("get auth path with special characters #2", {
+  bucket <- "made.up"
+  key <- "path/file#"
+  expect_out <- list(
+    sprintf("/%s", bucket),
+    sprintf("/%s", bucket),
+    sprintf("/%s/%s", bucket, paws.common:::paws_url_encoder(key, "/-")),
+    sprintf("/%s/%s", bucket, paws.common:::paws_url_encoder(key, "/~"))
+  )
+
+  for (i in seq_along(http_paths)) {
+    actual <- paws.common:::get_auth(mock_request(bucket, key, http_paths[i]))
+    expect_equal(actual, expect_out[[i]])
+  }
+})
+
+test_that("get auth path with small bucket name", {
+  bucket <- "hi"
+  key <- "path/file#"
+  expect_out <- list(
+    sprintf("/%s", bucket),
+    sprintf("/%s", bucket),
+    sprintf("/%s/%s", bucket, paws.common:::paws_url_encoder(key, "/-")),
+    sprintf("/%s/%s", bucket, paws.common:::paws_url_encoder(key, "/~"))
+  )
+
+  for (i in seq_along(http_paths)) {
+    actual <- paws.common:::get_auth(mock_request(bucket, key, http_paths[i]))
+    expect_equal(actual, expect_out[[i]])
+  }
+})
+
+test_that("check standard generated presigned url", {
+  Sys.setenv("AWS_ACCESS_KEY_ID" = "DUMMY")
+  Sys.setenv("AWS_SECRET_ACCESS_KEY" = "SECRETDUMMY")
+  Sys.setenv("AWS_REGION" = "us-east-1")
+
+  s3 <- list()
+  s3$generate_presigned_url <- paws.common:::generate_presigned_url
+  actual <- s3$generate_presigned_url(
+    client_method = "list_objects_v2",
+    params = list(Bucket = "foo", Prefix = "bar")
+  )
+
+  expect_true(
+    grepl(
+      sprintf(
+        "https://%s.s3.amazonaws.com/\\?list-type=2&prefix=%s&AWSAccessKeyId=%s&Expires=.*?&Signature=.*",
+        "foo",
+        "bar",
+        "DUMMY"
+      ),
+      actual
+    )
+  )
+})
+
+test_that("check generated presigned url with different http_method", {
+  Sys.setenv("AWS_ACCESS_KEY_ID" = "DUMMY")
+  Sys.setenv("AWS_SECRET_ACCESS_KEY" = "SECRETDUMMY")
+  Sys.setenv("AWS_REGION" = "us-east-1")
+
+  s3 <- list()
+  s3$generate_presigned_url <- paws.common:::generate_presigned_url
+  actual <- s3$generate_presigned_url(
+    client_method = "list_objects_v2",
+    params = list(Bucket = "foo", Prefix = "bar"),
+    http_method = "HTTP"
+  )
+
+  expect_true(
+    grepl(
+      sprintf(
+        "HTTP://%s.s3.amazonaws.com/\\?list-type=2&prefix=%s&AWSAccessKeyId=%s&Expires=.*?&Signature=.*",
+        "foo",
+        "bar",
+        "DUMMY"
+      ),
+      actual
+    )
+  )
+})
+
+test_that("check generate_presigned_url with error in client_method", {
+  Sys.setenv("AWS_ACCESS_KEY_ID" = "DUMMY")
+  Sys.setenv("AWS_SECRET_ACCESS_KEY" = "SECRETDUMMY")
+  Sys.setenv("AWS_REGION" = "us-east-1")
+
+  s3 <- list()
+  s3$generate_presigned_url <- paws.common:::generate_presigned_url
+  expect_error(
+    s3$generate_presigned_url(
+      client_method = "AWS_HELLO",
+      params = list(Bucket = "foo", Prefix = "bar")
+    ),
+    "Client does not have method: AWS_HELLO"
+  )
+})
+
+test_that("check generate_presigned_url with in correct parameters", {
+  Sys.setenv("AWS_ACCESS_KEY_ID" = "DUMMY")
+  Sys.setenv("AWS_SECRET_ACCESS_KEY" = "SECRETDUMMY")
+  Sys.setenv("AWS_REGION" = "us-east-1")
+
+  s3 <- list()
+  s3$generate_presigned_url <- paws.common:::generate_presigned_url
+  expect_error(
+    s3$generate_presigned_url(
+      client_method = "list_buckets",
+      params = list(MadeUp = "foo")
+    ),
+    "Invalid parameter.*"
+  )
+})
