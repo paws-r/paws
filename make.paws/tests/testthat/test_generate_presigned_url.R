fde269103c86d94f9972d092b3c02a717f870c7a
dyfan.jones <23235152+DyfanJones@users.noreply.github.com>
2022-12-29 14:58:19 +0000

    test s3_generate_presigned_url higher level functionality

---
+++ make.paws/tests/testthat/test_generate_presigned_url.R
@@ -0,0 +1,74 @@
+.s3 <- list()
+
+# get s3_generate_presigned_url custom s3 function
+custom_dir <- file.path(getwd(), "..", "..", "R", "custom")
+source(file.path(custom_dir, "s3.R"), local = TRUE)
+
+# import private methods from paws.common
+get_pkg_api <- get("get_pkg_api", asNamespace("paws.common"))
+get_config <- get("get_config", asNamespace("paws.common"))
+new_request <- get("new_request", asNamespace("paws.common"))
+build <- get("build", asNamespace("paws.common"))
+sign_v1_auth_query <- get("sign_v1_auth_query", asNamespace("paws.common"))
+build_url <- get("build_url", asNamespace("paws.common"))
+`%||%` <- get("%||%", asNamespace("paws.common"))
+
+test_that("check generated presigned url with different http_method", {
+  skip_if_not_installed("paws.common")
+  Sys.setenv("AWS_ACCESS_KEY_ID" = "DUMMY")
+  Sys.setenv("AWS_SECRET_ACCESS_KEY" = "SECRETDUMMY")
+  Sys.setenv("AWS_REGION" = "us-east-1")
+
+  s3 <- list()
+  s3$generate_presigned_url <- s3_generate_presigned_url
+  actual <- s3$generate_presigned_url(
+    client_method = "list_objects_v2",
+    params = list(Bucket = "foo", Prefix = "bar"),
+    http_method = "HTTP"
+  )
+
+  expect_true(
+    grepl(
+      sprintf(
+        "HTTP://%s.s3.amazonaws.com/\\?list-type=2&prefix=%s&AWSAccessKeyId=%s&Expires=.*?&Signature=.*",
+        "foo",
+        "bar",
+        "DUMMY"
+      ),
+      actual
+    )
+  )
+})
+
+test_that("check generate_presigned_url with error in client_method", {
+  skip_if_not_installed("paws.common")
+  Sys.setenv("AWS_ACCESS_KEY_ID" = "DUMMY")
+  Sys.setenv("AWS_SECRET_ACCESS_KEY" = "SECRETDUMMY")
+  Sys.setenv("AWS_REGION" = "us-east-1")
+
+  s3 <- list()
+  s3$generate_presigned_url <- s3_generate_presigned_url
+  expect_error(
+    s3$generate_presigned_url(
+      client_method = "AWS_HELLO",
+      params = list(Bucket = "foo", Prefix = "bar")
+    ),
+    "Client does not have method: AWS_HELLO"
+  )
+})
+
+test_that("check generate_presigned_url with wrong parameters", {
+  Sys.setenv("AWS_ACCESS_KEY_ID" = "DUMMY")
+  Sys.setenv("AWS_SECRET_ACCESS_KEY" = "SECRETDUMMY")
+  Sys.setenv("AWS_REGION" = "us-east-1")
+
+  s3 <- list()
+  s3$generate_presigned_url <- s3_generate_presigned_url
+  expect_error(
+    s3$generate_presigned_url(
+      client_method = "list_buckets",
+      params = list(MadeUp = "foo")
+    ),
+    "Invalid parameter.*"
+  )
+})
