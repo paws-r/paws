147e93c9f2897c4c1f1de1ecb6b6d3e07f6aefbd
dyfan.jones <dyfan.jones@theverygroup.com>
2023-05-02 17:34:33 +0100

    initial github_actions commands

--- make.paws/R/make_sdk_github_actions.R
+++ make.paws/R/make_sdk_github_actions.R
@@ -1,16 +1,16 @@
 #' @include cran_category.R cran_collection.R cran_sub_category.R
 
-#' @title Make the AWS SDK R package
-#'
-#' @param in_dir Directory containing AWS API input files.
-#' @param out_sdk_dir Directory to write the R packages to.
-#' @param out_doc_dir Directory to write the documentation to.
-#' @param apis An optional list of APIs to include.
-#' @param only_cran Whether to include only the R packages already on CRAN.
-#' @param cache_dir Directory to store cached artifacts. Providing a non-`NULL`
-#'   value will greatly speed up subsequent runs that use the same value.
-#' @name make_sdk
-#' @export
+# #' @title Make the AWS SDK R package
+# #'
+# #' @param in_dir Directory containing AWS API input files.
+# #' @param out_sdk_dir Directory to write the R packages to.
+# #' @param out_doc_dir Directory to write the documentation to.
+# #' @param apis An optional list of APIs to include.
+# #' @param only_cran Whether to include only the R packages already on CRAN.
+# #' @param cache_dir Directory to store cached artifacts. Providing a non-`NULL`
+# #'   value will greatly speed up subsequent runs that use the same value.
+# #' @name make_sdk
+# #' @export
 build_apis <- function(
     in_dir = "./vendor/aws-sdk-js",
     out_sdk_dir = "./cran",
@@ -18,11 +18,11 @@ build_apis <- function(
     apis = character(0),
     only_cran = TRUE,
     cache_dir = "./cache") {
-  
+
   if (length(apis) == 0) {
     apis <- list_apis(file.path(in_dir, "apis"))
   }
-  
+
   # Get the category-level packages that the SDK is separated into.
   # The SDK is separated into categories to fit in CRAN's package size limit.
   categories <- get_categories()
@@ -34,12 +34,12 @@ build_apis <- function(
     apis_to_use <- unlist(sapply(categories, function(x) x$services))
     apis <- apis[apis %in% apis_to_use]
   }
-  
+
   temp_dir <- file.path(tempdir(), "paws")
   dir.create(temp_dir, recursive = TRUE, showWarnings = FALSE)
-  
+
   api_names <- list()
-  
+
   # Generate the code and documentation.
   with_cache_dir(cache_dir, {
     clear_dir(temp_dir)
@@ -62,32 +62,28 @@ build_apis <- function(
 }
 
 
-github_make_category_collection <- function() {
+github_make_category_collection <- function(
+    out_sdk_dir = "./cran",
+    out_doc_dir = "./paws") {
+  # Get the category-level packages that the SDK is separated into.
+  # The SDK is separated into categories to fit in CRAN's package size limit.
+  categories <- get_categories()
+
   # Identify sub-categories
   found <- find_sub_categories(categories)
-  
+
   if (any(found)){
     # Group categories
     grp_sub_cats <- group_categories(categories[found])
-    
+
     # Build categories from sub-categories
     for(cat in names(grp_sub_cats)){
-      make_category_collection(temp_dir, out_sdk_dir, grp_sub_cats[[cat]], cat, api_names,  refresh = TRUE)
+      make_category_collection(out_doc_dir, out_sdk_dir, grp_sub_cats[[cat]], cat, api_names,  refresh = TRUE)
     }
   }
 }
-  
-github_make_collection <- function() {
+
+github_make_collection <- function(out_sdk_dir = "./cran",
+                                   out_doc_dir = "./paws") {
   make_collection(temp_dir, out_sdk_dir, categories, api_names,  refresh = TRUE)
 }
-
-
-
-
-
-
-
-
-
-
-
