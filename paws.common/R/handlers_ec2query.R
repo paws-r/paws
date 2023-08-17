e2bba1689023268f2051d29178b4ca11076e077e
dyfan.jones <dyfan.jones@theverygroup.com>
2023-08-17 19:19:33 +0100

    pass xml object to xml_parse

--- paws.common/R/handlers_ec2query.R
+++ paws.common/R/handlers_ec2query.R
@@ -21,9 +21,10 @@ ec2query_build <- function(request) {
 
 # Unmarshal the response from an EC2 protocol response.
 ec2query_unmarshal <- function(request) {
-  body <- decode_xml(request$http_response$body)
-  data <- body[[1]]
-  request$data <- xml_parse(data, request$data)
+  body <- xml2::read_xml(request$http_response$body, encoding = "utf8")
+  interface <- request$data
+  # data <- body[[1]]
+  request$data <- xml_parse(body, interface)
   return(request)
 }
 
