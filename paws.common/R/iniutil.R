c895c0a5fb52ced31bc8dc02be682ba5ccc1337c
dyfan.jones <dyfan.jones@theverygroup.com>
2023-10-12 09:53:04 +0100

    minor optimisation for large ini files

--- paws.common/R/iniutil.R
+++ paws.common/R/iniutil.R
@@ -2,13 +2,12 @@
 
 # Get a parameter and its value
 extract_ini_parameter <- function(item) {
-  item <- trimws(item)
+  item <- gsub("^[ \t\r\n]+|[ \t\r\n]+$", "", item, perl = T)
   parameter <- list(item[2])
   names(parameter) <- item[1]
   return(parameter)
 }
 
-# Read in values from an ini file
 read_ini <- function(file_name) {
   if (!file.exists(file_name)) {
     stopf("Unable to find file: %s", file_name)
@@ -37,15 +36,16 @@ read_ini <- function(file_name) {
   start <- (found + 1)
   end <- c(found[-1]-1,  length(content))
   split_content <- strsplit(sub("=", "\n", content, fixed = T), "\n", fixed = T)
-  for (i in which(start <= end)) {
-    sub_content <- split_content[seq.int(start[i], end[i])]
-    found_nested_content <- lengths(sub_content) == 1
+  nested_contents <- lengths(split_content) == 1
 
+  for (i in which(start <= end)) {
+    items <- seq.int(start[i], end[i])
+    found_nested_content <- nested_contents[items]
     if (any(found_nested_content)) {
-      profiles[[i]] <- nested_ini_content(sub_content, found_nested_content)
+      profiles[[i]] <- nested_ini_content(split_content[items], found_nested_content)
     } else {
       profiles[[i]] <- unlist(lapply(
-        sub_content, extract_ini_parameter
+        split_content[items], extract_ini_parameter
       ), recursive = F
       )
     }
@@ -58,10 +58,10 @@ nested_ini_content <- function(sub_content, found_nested_content) {
   start <- (position + 1)
   end <- c(position[-1]-1,  length(sub_content))
 
-  profile_nms <- trimws(unlist(sub_content[position]))
+  profile_nms <- gsub("^[ \t\r\n]+|[ \t\r\n]+$", "", unlist(sub_content[position]), perl = T)
 
   sub_grp <- which(!vapply(sub_content, function(x) grepl("^[ ]+", x[1]), logical(1)))
-  non_nest <- sub_grp[!sub_grp%in% position]
+  non_nest <- sub_grp[!sub_grp %in% position]
 
   profiles <- unlist(lapply(
     sub_content[non_nest], extract_ini_parameter
