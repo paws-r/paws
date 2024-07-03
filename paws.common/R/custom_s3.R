7ecbd1c774e3be029f55a82dcd43cd97d2efa40c
dyfan.jones <dyfan.jones@theverygroup.com>
2024-07-03 18:15:42 +0100

    add final redirect attempt

--- paws.common/R/custom_s3.R
+++ paws.common/R/custom_s3.R
@@ -2,6 +2,7 @@
 #' @include stream.R
 #' @include util.R
 #' @include error.R
+#' @include head_bucket.R
 NULL
 
 ################################################################################
@@ -348,7 +349,7 @@ s3_redirect_from_error <- function(request) {
     return(request)
   }
   bucket_name <- request$params[["Bucket"]]
-  new_region <- s3_get_bucket_region(request$http_response, error)
+  new_region <- s3_get_bucket_region(request, error, bucket_name)
   if (is.null(new_region)) {
     log_debug(
       paste(
@@ -429,14 +430,24 @@ can_be_redirected <- function(request, error_code, error) {
 # HEAD on the bucket if all else fails.
 # param response: HttpResponse
 # param error: Error
-s3_get_bucket_region <- function(response, error) {
+s3_get_bucket_region <- function(request, error, bucket) {
   # First try to source the region from the headers.
-  response_headers <- response$header
+  response_headers <- request$http_response$header
   if (!is.null(region <- response_headers[["x-amz-bucket-region"]])) {
     return(unlist(region))
   }
-  # Next, check the error body
-  return(unlist(error$Region))
+  if (!is.null(region <- unlist(error$Region))) {
+    return(region)
+  }
+
+  # Finally, HEAD the bucket. No other choice sadly.
+  tryCatch({
+    client <- s3(request$config)
+    resp <- client$head_bucket(Bucket = bucket)
+  })
+  region <- resp$BucketRegion
+
+  return(region)
 }
 
 # Splice a new endpoint into an existing URL. Note that some endpoints
---
+++ paws.common/R/head_bucket.R
@@ -0,0 +1,53 @@
+s3 <- function(config = list()) {
+  svc <- .s3$operations
+  svc <- set_config(svc, config)
+  return(svc)
+}
+
+.s3 <- list()
+
+.s3$operations <- list()
+
+.s3$metadata <- list(
+  service_name = "s3",
+  endpoints = list("us-gov-west-1" = list(endpoint = "s3.{region}.amazonaws.com", global = FALSE), "us-west-1" = list(endpoint = "s3.{region}.amazonaws.com", global = FALSE), "us-west-2" = list(endpoint = "s3.{region}.amazonaws.com", global = FALSE), "eu-west-1" = list(endpoint = "s3.{region}.amazonaws.com", global = FALSE), "ap-southeast-1" = list(endpoint = "s3.{region}.amazonaws.com", global = FALSE), "ap-southeast-2" = list(endpoint = "s3.{region}.amazonaws.com", global = FALSE), "ap-northeast-1" = list(endpoint = "s3.{region}.amazonaws.com", global = FALSE), "sa-east-1" = list(endpoint = "s3.{region}.amazonaws.com", global = FALSE), "us-east-1" = list(endpoint = "s3.amazonaws.com", global = FALSE), "*" = list(endpoint = "s3.{region}.amazonaws.com", global = FALSE), "cn-*" = list(endpoint = "s3.{region}.amazonaws.com.cn", global = FALSE), "eu-isoe-*" = list(endpoint = "s3.{region}.cloud.adc-e.uk", global = FALSE), "us-iso-*" = list(endpoint = "s3.{region}.c2s.ic.gov", global = FALSE), "us-isob-*" = list(endpoint = "s3.{region}.sc2s.sgov.gov", global = FALSE), "us-isof-*" = list(endpoint = "s3.{region}.csp.hci.ic.gov", global = FALSE)),
+  service_id = "S3",
+  api_version = "2006-03-01",
+  signing_name = "s3",
+  json_version = "",
+  target_prefix = ""
+)
+
+.s3$service <- function(config = list()) {
+  handlers <- new_handlers("restxml", "s3")
+  new_service(.s3$metadata, handlers, config)
+}
+
+.s3$head_bucket_input <- function(...) {
+  args <- c(as.list(environment()), list(...))
+  shape <- structure(list(Bucket = structure(logical(0), tags = list(location = "uri", locationName = "Bucket", type = "string")), ExpectedBucketOwner = structure(logical(0), tags = list(location = "header", locationName = "x-amz-expected-bucket-owner", type = "string"))), tags = list(type = "structure"))
+  return(populate(args, shape))
+}
+
+.s3$head_bucket_output <- function(...) {
+  args <- c(as.list(environment()), list(...))
+  shape <- structure(list(BucketLocationType = structure(logical(0), tags = list(location = "header", locationName = "x-amz-bucket-location-type", type = "string")), BucketLocationName = structure(logical(0), tags = list(location = "header", locationName = "x-amz-bucket-location-name", type = "string")), BucketRegion = structure(logical(0), tags = list(location = "header", locationName = "x-amz-bucket-region", type = "string")), AccessPointAlias = structure(logical(0), tags = list(location = "header", locationName = "x-amz-access-point-alias", type = "boolean", box = TRUE))), tags = list(type = "structure"))
+  return(populate(args, shape))
+}
+
+s3_head_bucket <- function(Bucket, ExpectedBucketOwner = NULL) {
+  op <- new_operation(
+    name = "HeadBucket",
+    http_method = "HEAD",
+    http_path = "/{Bucket}",
+    paginator = list()
+  )
+  input <- .s3$head_bucket_input(Bucket = Bucket, ExpectedBucketOwner = ExpectedBucketOwner)
+  output <- .s3$head_bucket_output()
+  config <- get_config()
+  svc <- .s3$service(config)
+  request <- new_request(svc, op, input, output)
+  response <- send_request(request)
+  return(response)
+}
+.s3$operations$head_bucket <- s3_head_bucket
