89e58dc9722843948dcb1c3db1696500d0f3c4df
dyfan.jones <dyfan.jones@theverygroup.com>
2024-07-30 10:25:07 +0100

    tidy up

--- paws.common/R/time.R
+++ paws.common/R/time.R
@@ -1,5 +1,5 @@
 # Returns a date value, given the number of seconds since Jan 1, 1970.
 unix_time <- function(sec, nsec = 0) {
   # origin: 1970-01-01 00:00:00
-  return(.POSIXct(0, tz = "GMT") + as.numeric(sec))
+  return(.POSIXct(as.numeric(sec), tz = "GMT"))
 }
