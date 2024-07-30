855127d599b20f26d79cb01ed899dd437ae81b1f
dyfan.jones <dyfan.jones@theverygroup.com>
2024-07-30 10:17:46 +0100

    simplify unix time

--- paws.common/R/time.R
+++ paws.common/R/time.R
@@ -1,5 +1,5 @@
 # Returns a date value, given the number of seconds since Jan 1, 1970.
 unix_time <- function(sec, nsec = 0) {
-  time <- ISOdatetime(1970, 1, 1, 0, 0, 0, tz = "GMT") + as.numeric(sec)
-  return(time)
+  # origin: 1970-01-01 00:00:00
+  return(.POSIXct(0, tz = "GMT") + as.numeric(sec))
 }
