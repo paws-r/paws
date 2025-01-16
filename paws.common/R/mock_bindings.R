49d70bf7d8f07fdb1e1b04886417a5572399bd00
dyfan.jones <dyfan.jones@theverygroup.com>
2025-01-16 18:10:11 +0000

    add base function bindings for mocking

---
+++ paws.common/R/mock_bindings.R
@@ -0,0 +1,6 @@
+################################################################
+# Base function binding for mocking:
+# https://testthat.r-lib.org/reference/local_mocked_bindings.html#base-functions
+################################################################
+
+Sys.time <- NULL
