c2f78d488c2f2560dc800df3babb46fde49eef5d
dyfan.jones <dyfan.jones@theverygroup.com>
2025-01-18 16:23:38 +0000

    add emptyenv as parent environment

--- paws.common/R/cache.R
+++ paws.common/R/cache.R
@@ -4,7 +4,7 @@ ini_cache <- new.env(parent = emptyenv())
 
 os_env_cache <- new.env(parent = emptyenv())
 
-vendor_cache <- list2env(list(vendor = "boto"))
+vendor_cache <- list2env(list(vendor = "boto"), parent = emptyenv())
 
 set_os_env_cache <- function() {
   env_vars <- system("printenv", intern = TRUE)
